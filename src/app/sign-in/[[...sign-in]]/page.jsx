dceacfd710d4befdbfc10d21fc0ebfd8720c7798
Gurusharan Singh <gurusharansingh2222@gmail.com>
2025-03-28 13:06:44 +0530

    Job dsco

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <SignIn></SignIn>
+    <div className='w-screen  flex justify-center items-center'><SignIn></SignIn></div>
+    
   )
 }
 
